08de46f17a3e71d6a8ed0db09c9ab2187e1850b6
StrawberryIdiot <72462624+StrawberryIdiot@users.noreply.github.com>
2020-10-20 09:28:36 -0700

    Add files via upload

---
+++ while.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# while-loop.py
+count = 0
+while(count < 256):
+  print(str(count) + " * ",end="")
+  count = count + 1
+  if(count % 20 == 0):
+    print(" * ")
+'''
+count increment
+encapsulated block - scope
+type cast
+concatenation
+% modulus modulo
+'''
